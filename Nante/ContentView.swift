442d831b961263418cb22364c6baa4638c8f95af
dangoMM <ocean.in.the.valley@gmail.com>
2023-08-27 00:58:06 +0900

    Initial Commit

---
+++ Nante/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nante
+//
+//  Created by 谷内洋介 on 2023/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nante/NanteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanteApp.swift
+//  Nante
+//
+//  Created by 谷内洋介 on 2023/08/27.
+//
+
+import SwiftUI
+
+@main
+struct NanteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
