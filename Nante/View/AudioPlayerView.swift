a9c79c325e9082f3f3cf5e373ba53c1e0db40ee6
dangoMM <ocean.in.the.valley@gmail.com>
2023-09-04 21:42:25 +0900

    merge audio player

--- Nante/View/AudioPlayerView.swift
+++ Nante/View/AudioPlayerView.swift
@@ -1,22 +1,50 @@
 //
-//  AudioPlayer.swift
+//  AudioPlayerView.swift
 //  Nante
 //
-//  Created by 谷内洋介 on 2023/09/03.
+//  Created by 谷内洋介 on 2023/08/28.
 //
 
 import SwiftUI
+import AVFoundation
 
 struct AudioPlayerView: View {
-    @EnvironmentObject var currentState: CurrentState
+    @State private var audioURLString: String = ""
+    @ObservedObject private var audioPlayer = AudioPlayer()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("音声ストリームのURLを入力してください", text: $audioURLString)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            HStack {
+                Button(action: {
+                    audioPlayer.rewind(by: 10.0)
+                }) {
+                    Image(systemName: "gobackward.10")
+                }
+                .padding()
+
+                Button(action: {
+                    if audioPlayer.isPlaying {
+                        audioPlayer.pause()
+                    } else if let url = URL(string: audioURLString) {
+                        audioPlayer.play(url: url)
+                    }
+                }) {
+                    Image(systemName: audioPlayer.isPlaying ? "pause.fill" : "play.fill")
+                }
+                .padding()
+
+                Button(action: {
+                    audioPlayer.forward(by: 10.0)
+                }) {
+                    Image(systemName: "goforward.10")
+                }
+                .padding()
+            }
+        }
     }
 }
 
-//struct AudioPlayer_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AudioPlayerView(contentState: .player)
-//    }
-//}
